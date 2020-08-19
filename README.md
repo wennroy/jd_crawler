5423fdc72ad56ad299aa4419df04a4559a8f79e7
wennroy <wennroy@gmail.com>
2020-08-19 17:16:04 +0800

    uploads

---
+++ README.md
@@ -0,0 +1,83 @@
+# Jd_Crawler
+
+## 简介
+
+利用Scrapy模块爬取京东商品信息
+
+爬取京东的商品信息，**输入**为
+
+| 名称           | 变量类型     | 描述                                              |
+| -------------- | ------------ | ------------------------------------------------- |
+| Search_name    | str          | 输入你想要搜索的东西，例如“苹果”、“iphone”等      |
+| total_page     | int          | 输入想要爬取的页数                                |
+| activate_mysql | `True/False` | 是否启用mysql进行存储，细节见`lenovo/__init__.py` |
+| database       | str          | 输入使用的数据库名称                              |
+
+**输出**为：
+
+| 名称         | 描述                                                         |
+| ------------ | ------------------------------------------------------------ |
+| id           | 爬取的按照顺序存储的id(Auto_increment, int, primary key)     |
+| item_id      | 京东商品的内部id，可直接利用https://item.jd.com/100011386554.html(左侧一串数目即为id)进行访问 |
+| item_name    | 商品名称                                                     |
+| price        | 商品价格                                                     |
+| shopname     | 商铺名称                                                     |
+| counting*     | 计算在爬取过程中该商品重复了几次                             |
+| comment_num  | 评论数量                                                     |
+| start_time   | 开始爬取的时间                                               |
+| end_time     | 存储的时间                                                   |
+| create_time* | 该条目被创建的时间                                           |
+| update_time* | 更新该条目的时间(例如再次碰到同样的商品时，就会更新该时间)   |
+
+*仅mysql数据库有。
+
+### 如何使用
+
+若要更改爬取的信息，打开`lenovo/__init__.py`，更改里面对应的参数即可。一般利用pycharm直接打开，直接运行`lenovo\spiders\quotes_spider.py`也可以，只不过文件会保存在spiders文件夹下。
+
+
+
+利用cmd或是anaconda(需安装scrapy和itemadapter等模块)，进入(cd)到 Jd 文件目录下，然后输入
+
+```
+scrapy crawl lenovo
+```
+
+由于一开始用lenovo试手，spiders的名字就叫lenovo了。
+
+
+
+## 其余文件解释
+
+一些csv文件为直接试爬取的结果。
+
+顺带附上`__init__.py`文件：
+
+```python
+Search_name = '苹果' ## 输入你想要搜索的东西，之后的表格、csv文件等将会以这个名字命名
+total_page = 2 ## 输入需要爬取的页数
+'''
+默认采取0.3秒的请求延迟，也就是每件物品需要0.3秒的延迟。15页物品大概需要5-6分钟。
+经测试，若不给予延迟，可能会导致丢页。若需修改，请至settings.py中修改
+DOWNLOAD_DELAY = .3
+'''
+
+'''
+是否启用mysql数据库，默认采用本地数据库ip:127.0.0.1,port = 3306
+若需更改连接ip请至pipelines.py中修改。并填写写入的数据库名称。必须已有该数据库。
+'''
+activate_mysql = True
+database = 'lenovo'
+'''
+最后写入csv文件，有部分文字因格式问题，无法写入。最终会打印出来，请自行复制填写。mysql没有该问题。
+'''
+
+```
+
+scrapy的具体教程见http://docs.scrapy.org/en/latest。
+
+
+
+# 写在最后
+
+这个爬虫就是写着玩玩练练手，运行不起来自己调试一下吧。反正我这里挺正常的。
---
+++ lenovo/spiders/quotes_spider.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 16 09:07:36 2020
+
+@author: lengwaifang
+"""
+
+import scrapy
+from lenovo.items import LenovoItem
+from scrapy.utils.project import get_project_settings
+from scrapy.crawler import CrawlerProcess
+import re
+import datetime
+import urllib
+from lenovo.__init__ import *
+
+class QuotesSpider(scrapy.Spider):
+    search_name = urllib.parse.quote(Search_name)
+    name = "lenovo"
+    item_list = []
+    show_item_id = []
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+        'referer': 'https://search.jd.com/Search?keyword=%E8%81%94%E6%83%B3&qrst=1&wq=%E8%81%94%E6%83%B3&ev=exbrand_%E8%81%94%E6%83%B3%EF%BC%88Lenovo%EF%BC%89%5E&page=1&s=1&click=0'
+    }
+    def start_requests(self):
+        for x in range(total_page):
+            item_num = 1 + x * 60
+            page_num = 2 * x + 1  # 's_new.php?'
+            url = 'https://search.jd.com/s_new.php?keyword=' + self.search_name + '&wq='+self.search_name + \
+                  '&s=' + str(item_num) + '&page=' + \
+                  str(page_num) + '&click=0'
+            referer_url = 'https://search.jd.com/Search?keyword=' + self.search_name+ '&wq='+self.search_name + \
+                          str(page_num) + '&s=' + str(item_num) + '&click=0'
+            self.headers['referer'] = referer_url
+            yield scrapy.Request(url=url, dont_filter=True, callback=self.parse, headers=self.headers,
+                                 meta={'page': page_num, 'item': item_num, 'retry': 0})
+
+    ## /div[@class='p-name p-name-type-2']/a/em
+    ## /div[@class='p-name p-name-type-2']/a/em
+    def parse(self, response):
+        self.show_item_id = []
+        retry = response.meta['retry']
+        all_item = response.xpath("//div[@class='gl-i-wrap']")
+        if len(all_item) == 0:
+            retry += 1
+            print(f'重试了{retry}次，警告：重试5次以上将会跳过第{response.meta["page"]}页的搜索。')
+            if not retry >5:
+                yield scrapy.Request(url=response.url, dont_filter=True, callback=self.parse, headers=self.headers,
+                                     meta={'page': response.meta['page'], 'item': response.meta['item'], 'retry': retry})
+
+        for content in all_item:
+            num_str = content.xpath(".//div[@class='p-commit']/strong/a/@id").get()
+            item_id = re.search(r"\d+", num_str).group(0)
+            id_url = 'https://club.jd.com/comment/productCommentSummaries.action?referenceIds=' + str(item_id)
+            self.show_item_id.append(item_id)
+            # if content.xpath(".//a[@class='curr-shop hd-shopname']/text()").get() == '立诗文二手笔记本专营店':
+            #     print(content.xpath(".//div[@class='p-name p-name-type-2']/a/em/text()").get())
+                ## '<em><font class="skcolor_ljg">联想</font>拯救者Y7000P 2020新品英特尔10代酷睿i7电竞屏吃鸡游戏本 15.6英高色域笔记本电脑 标配【i7-10875H 16G内存 512固态】 RTX2060/GTX1660Ti 6G钛晶灰</em>'
+                ##<em><span class="p-tag" style="background-color:#c81623">京品电脑</span>\t\n<font class="skcolor_ljg">联想</font>小新Air14 2020锐龙版(全新7nm)六核金属超轻薄笔记本电脑 学生本商务设计游戏轻薄本 标配【R5 4600U 16G内存 512固态】灰</em>'
+            item_name = ''
+            for i in content.xpath(".//div[@class='p-name p-name-type-2']/a/em/text()").getall():
+                item_name += i
+            item_name = item_name.strip()
+            # item_name.replace('<em><font class="skcolor_ljg">联想</font>','联想')
+            # item_name.replace('</em>','')
+            if re.findall(r'[\u4E00-\u9FA5A-Za-z0-9_]+', item_name) == []:
+                item_name = content.xpath(".//div[@class='p-name p-name-type-2']/a/em").get()
+                item_name = item_name[item_name.find('</font>') + 7:-5]
+            item_name = item_name.strip()
+            temp_dict = {'price': content.xpath(".//div[@class='p-price']//i/text()").get(),
+                         'name': item_name,
+                         'shopname': content.xpath(".//a[@class='curr-shop hd-shopname']/text()").get(),
+                         'item_id': item_id}
+            yield scrapy.Request(id_url, dont_filter=True, meta=temp_dict, callback=self.parse_2)
+
+        print(self.show_item_id)
+        if response.url.find('show_items') == -1:
+            page_num = 1 + response.meta['page']
+            item_num = 30 + response.meta['item']
+            show_item_str = ''
+            for x in self.show_item_id:
+                show_item_str += str(x) + ','
+            show_item_str = show_item_str[:-1]
+            next_url = 'https://search.jd.com/s_new.php?keyword=' + self.search_name + '&wq=' + self.search_name + \
+                  '&s=' + str(item_num) + '&page=' + \
+                  str(page_num) + '&click=0' + '&show_items=' + show_item_str
+            self.headers[
+                'referer'] = 'https://search.jd.com/Search?keyword='+self.search_name+'&wq=' + self.search_name + \
+                          str(page_num) + '&s=' + str(item_num) + '&click=0'
+            yield scrapy.Request(next_url, dont_filter=True, callback=self.parse, headers=self.headers,
+                                 meta={'page': page_num, 'item': item_num, 'retry': 0})
+
+    def parse_2(self, response):
+        import json
+        comment_dict = json.loads(response.text)
+        comment_num = comment_dict['CommentsCount'][0]['CommentCountStr']
+        item = LenovoItem()
+        item['start_time'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        item['name'] = response.meta['name']
+        item['price'] = response.meta['price']
+        item['shopname'] = response.meta['shopname']
+        item['num_comment'] = comment_num
+        item['item_id'] = response.meta['item_id']
+        yield item
+
+
+if __name__ == "__main__":
+    cp = CrawlerProcess(get_project_settings())
+    cp.crawl(QuotesSpider)
+    cp.start()
